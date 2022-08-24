098ba63b0fa2c85a87363a45576a3fe9882ca806
EricZhangXu <eric.zhang@3plearning.com>
2022-08-24 10:26:12 +1000

    Init Project and add logging support

---
+++ NET6ConsoleAPPWithDI/NET6ConsoleAPPWithDI/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace NET6ConsoleAPPWithDI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var hostBuilder = CreateHostBuilder(args);;
+                var host = hostBuilder.Build();
+                var provider = host.Services;
+
+                var logger = provider.GetRequiredService<ILogger<Program>>();
+                logger.LogInformation("App starts......");
+
+
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var hostBuilder = Host.CreateDefaultBuilder(args)
+            .ConfigureServices((context, services) =>
+            {
+                services.AddLogging(loggingBuilder => loggingBuilder.AddConsole());
+            });
+
+            return hostBuilder;
+        }
+
+    }
+}
